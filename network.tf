5a95ba6f7789bdd0905478be55574b6d30b6611c
Taro Murata <taro.murata@hashicorp.com>
2022-12-21 05:36:36 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/tarohcp/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "${var.prefix}"-work
+  resource_group_name = "${var.prefix}-work"
 }
